5c06a4201772ecd0f530282a6674c13e1a3a9f8c
hyein7447 <hyein7447@gmail.com>
2024-02-16 16:21:35 +0900

    위시리스트 1280작업중

---
+++ script/header.js
@@ -0,0 +1,16 @@
+// 카테고리 icon 마우스 오버 시 gnb 출력
+
+const category = document.querySelector('.category')
+const gnb = document.querySelector('.gnb')
+const gnb_bg = document.querySelector('.gnb_bg')
+console.log(gnb)
+
+gnb_bg.style.display = 'none'
+// gnb.style.display = 'none'
+
+category.addEventListener('mouseover',()=>{
+    gnb_bg.style.display = 'block'
+})
+category.addEventListener('mouseout',()=>{
+    gnb_bg.style.display = 'none'
+})
