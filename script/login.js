0bec8b744bfc86cc54ac1c535dbd6b8fbbd52b37
eunji <dmswl7954@gmail.com>
2024-02-22 01:51:48 +0900

    로그인페이지 자바스크립트 시작

---
+++ script/login.js
@@ -0,0 +1,4 @@
+const popup_box = document.querySelector('.box')
+const popup_message = document.querySelector('.box > p')
+const pop_btn_ok = document.querySelector('.box > .btn_ok')
+const btn_login = document.querySelector('.btn_login')
